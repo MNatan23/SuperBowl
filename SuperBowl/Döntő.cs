f6655c667f57aa058e512fd884a8e6d1d39155f3
MNatan23 <163414251+MNatan23@users.noreply.github.com>
2025-01-06 09:40:11 +0100

    4

---
+++ SuperBowl/Döntő.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBowl
+{
+    internal class Döntő
+    {
+        public string sSz { get; set; }
+        public string Dátum { get; set; }
+        public string Győztes { get; set; }
+        public string Eredmény { get; set; }
+        public string Vesztes { get; set; }
+        public string Helyszín { get; set; }
+        public string VárosÁllam { get; set; }
+        public int Nézőszám { get; set; }
+
+        public Döntő(string sor)
+        {
+            string[ ] s =  sor.Split(';');
+            sSz = s[0];
+            Dátum = s[1];
+            Győztes = s[2];
+            Eredmény = s[3];
+            Vesztes = s[4];
+            Helyszín = s[5];
+            VárosÁllam = s[6];
+            Nézőszám = int.Parse(s[7]);
+        }
+    }
+}
